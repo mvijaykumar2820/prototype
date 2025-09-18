0bcccd1e2bb22a26195d65528ecf5eb5e87b370b
Moodavath vijay kumar <mvijaygithub@gmail.com>
2025-09-19 00:08:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/prototype/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -15,5 +15,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/prototype/", // <-- Add this line for GitHub Pages
 }));
